770df6d1caf9a6896df6b25448cf217f0d18c703
João Pedro Patrocinio <jpedro9966@gmail.com>
2023-07-14 16:59:37 -0300

    Add timestamp and content printing

--- src/main.py
+++ src/main.py
@@ -1,10 +1,16 @@
 from flask import Flask
 from flask import request
+import datetime
 
 testapp = Flask(__name__)
 
 @testapp.get('/')
 def index():
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print(f'[{timestamp}] {request.remote_addr} {request.method} {request.path}')
+    print("--- CONTENT START ---")
+    print(request.headers)
+    print("--- CONTENT END ---")
     return 'OK'
 
 if __name__ == '__main__':
